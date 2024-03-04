dba6975639d10cd6c0ed99d0420112e8fab57ab2
emilianogastaldo <emiliano0303@gmail.com>
2024-03-04 15:55:39 +0100

    JOIN 4

--- solution.sql
+++ solution.sql
@@ -57,7 +57,8 @@ SELECT * FROM `departments` AS DEP JOIN `degrees` AS DEG ON DEG.`department_id` 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 SELECT * FROM `teachers` JOIN `course_teacher` ON `teachers`.`id` = `course_teacher`.`teacher_id` JOIN `courses` ON `courses`.`id` = `course_teacher`.`course_id` WHERE `teachers`.`name` = 'Fulvio' AND `teachers`.`surname` = 'Amato' AND `teachers`.`id` = 44;
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
-
+SELECT `students`.`id`, `students`.`surname`,`students`.`name`, `degrees`.`name` AS 'Corso di Laurea', `departments`.`name` AS 'Dipartimento' FROM `students` JOIN `degrees` ON `students`.`degree_id` = `degrees`.`id` JOIN `departments` ON `degrees`.`department_id` = `departments`.`id` ORDER BY `students`.`surname` ASC;
+SELECT `students`.`id`, `students`.`name`,`students`.`surname`, `degrees`.`name` AS 'Corso di Laurea', `departments`.`name` AS 'Dipartimento' FROM `students` JOIN `degrees` ON `students`.`degree_id` = `degrees`.`id` JOIN `departments` ON `degrees`.`department_id` = `departments`.`id` ORDER BY `students`.`name` ASC;
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
